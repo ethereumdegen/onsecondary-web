e7d51f411dc870af492e06dbc4082a472fad4d66
Admazzola <admazzola@gmail.com>
2021-12-26 17:29:28 -0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -77,8 +77,8 @@
 
                 <ul class=" "> 
                     <li class="my-4"> 1. Stake SOS in the Club SOS Contract </li>
-                    <li class="my-4" > 2. Neutral Dapps/Contracts donate some of their income to the Contract as revenue-share to use as a marketing+branding boost </li>
-                    <li class="my-4" > 3. Unstake at any time to withdraw <span class="text-purple-300"> your original deposit + accrued donations </span>  </li>
+                    <li class="my-4" > 2. Web3 applications donate some of their income to the Contract as revenue-share to use as a marketing+branding boost to all SOS holders </li>
+                    <li class="my-4" > 3. Unstake SOS at any time to withdraw <span class="text-purple-300"> your original deposit + accrued donations </span>  </li>
                </ul>
 
                 <div class="text-lg text-center my-16  "> TLDR: This is donations router for community-driven marketing and incentivization. </div>
