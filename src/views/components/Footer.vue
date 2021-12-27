e7bcb8ce2024b386298b85986b01e28a9a8d2f9a
andy <admazzola@gmail.com>
2021-12-27 00:24:26 -0500

    update foote

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -7,8 +7,8 @@
         <div class="w-1/2 lg:w-1/4 flex flex-col">
           <h3 class=" text-lg font-bold ">Documentation</h3>
 
-          <a href="https://etherscan.io/address/0xf14f78349882254e4d52cf24d9c5d3fd219d68c0" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300 ">Club Contract</a>
-           <a href="https://etherscan.io/address/0x111816fef278dff75aa27b5e03dfa74c73c0e22d" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">Reserve Token</a>
+          <a href="https://etherscan.io/address/0xdb3b9730353232d972b4e0ceff1297b6760d127b" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300 ">Club Contract</a>
+           <a href="https://etherscan.io/address/0xd628f9ff5bbf1cd61acc1b7b9d6dc7798a1c33f2" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">Reserve Token</a>
               
             <a href="https://github.com/admazzola/clubsos-web" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">Web Code</a>
              
