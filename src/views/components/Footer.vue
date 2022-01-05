e90bccb003b958a6923dccc99904f1f9625f3db9
andy <admazzola@gmail.com>
2022-01-05 13:43:56 -0500

    footer

--- src/views/components/Footer.vue
+++ src/views/components/Footer.vue
@@ -10,8 +10,8 @@
       <a href="https://docs.onsecondary.com" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">Official Docs</a>
          
 
-          <a href="https://etherscan.io/address/0xdb3b9730353232d972b4e0ceff1297b6760d127b" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300 ">Club Contract</a>
-           <a href="https://etherscan.io/address/0xd628f9ff5bbf1cd61acc1b7b9d6dc7798a1c33f2" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">Reserve Token</a>
+          <a href="https://etherscan.io/address/0xdb3b9730353232d972b4e0ceff1297b6760d127b" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300 ">Rev Share Contract</a>
+           <a href="https://etherscan.io/address/0xd628f9ff5bbf1cd61acc1b7b9d6dc7798a1c33f2" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">Rev Share Token</a>
               
            
               
@@ -19,7 +19,7 @@
         <div class="w-1/2 lg:w-1/4 flex flex-col">
           <h3 class=" text-lg font-bold ">Community &amp; Social</h3>
           
-            <a href="https://discord.gg/mUPRJbpMsG" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">OnSecondary Discord</a>
+            <a href="discord.onsecondary.com" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">OnSecondary Discord</a>
             <a href="https://twitter.com/onsecondarynft" target="_blank" class="no-underline inline-block text-sm text-gray-200 hover:text-purple-300">OnSecondary Twitter</a>
             
          </div>
